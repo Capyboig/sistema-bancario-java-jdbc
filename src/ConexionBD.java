cfdef2576515d7e5881310333f799b80180eae27
Capybara <cisco24javier@gmail.com>
2026-04-12 18:14:37 +0200

    Update ConexionBD.java

--- src/ConexionBD.java
+++ src/ConexionBD.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/banco_db";
-    private static final String USER = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = "";
     private static final String PASS = "";
 
     public static Connection conectar() {
